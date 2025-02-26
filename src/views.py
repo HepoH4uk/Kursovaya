4d2afac82048cbca2fe8af6a1c80c1a0be460fa5
AlTim <aleks00_777@mail.ru>
2025-02-26 19:01:56 +0300

    add views.py

---
+++ src/views.py
@@ -0,0 +1,51 @@
+from src.utils import (greetings, exchange_rate, get_price_sp500, max_five_transactions, user_transactions)
+from typing import Union
+import pandas as pd
+import datetime
+from pathlib import Path
+from dotenv import load_dotenv
+
+
+load_dotenv('../.env')
+
+current_dir = Path(__file__).parent.parent.resolve()
+
+dir_transactions_excel = current_dir/'data'/'operations.xlsx'
+print(dir_transactions_excel)
+
+
+def website(data_time: datetime) -> Union[list, dict]:
+
+    """
+    Главная функция, принимающую на вход строку с датой и временем в формате
+    YYYY-MM-DD HH:MM:SS и возвращающую JSON-ответ со следующими данными:
+    Приветствие в формате "???", где ??? — «Доброе утро» / «Добрый день» / «Добрый вечер» / «Доброй ночи» в зависимости
+    от текущего времени.
+    По каждой карте:
+    последние 4 цифры карты;
+    общая сумма расходов;
+    кешбэк (1 рубль на каждые 100 рублей).
+    Топ-5 транзакций по сумме платежа.
+    Курс валют.
+    Стоимость акций из S&P500.
+    """
+    print(f"Входные данные: {data_time}")
+    result1 = greetings()
+    result2 = user_transactions(data_time)
+    result3 = max_five_transactions(data_time)
+    result4 = exchange_rate()
+    result5 = get_price_sp500()
+
+    return result1, result2, result3, result4, result5
+
+
+if __name__ == '__main__':
+
+    print(f'{greetings()}')
+    data_time = pd.Timestamp("31-12-2021 00:00:00")
+    result = user_transactions(data_time)
+    print("Результат транзакций:")
+    print(result)
+    print(f"Пять максимальных транзакций: {max_five_transactions(data_time)}")
+    print(f"Курс валют: {exchange_rate()}")
+    print(f"Стоимость акций из SP500: {get_price_sp500()}")
